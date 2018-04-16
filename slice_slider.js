4739cee4cbe10bd5b66ee30b6e6d3951f0d57ee4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-16 11:28:21 -0400

    Bronze Template 1.0  /slice_slider.js

--- slice_slider.js
+++ slice_slider.js
@@ -1,4 +1,14 @@
-(function($) {
+;(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+
+}(function($) {
  
   var SliceSlider = {
     
