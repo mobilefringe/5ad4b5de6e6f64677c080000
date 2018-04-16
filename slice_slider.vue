6ccdcda1e354916d1778e906dca19fd29251cd7a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-16 14:44:41 -0400

    Bronze Template 1.0  /slice_slider.vue

--- slice_slider.vue
+++ slice_slider.vue
@@ -146,28 +146,28 @@
                      */
                     handleScroll: function(e){
                 
-                      // Scrolling up
-                      if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) { 
+                    //   // Scrolling up
+                    //   if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) { 
                 
-                        s.delta--;
+                    //     s.delta--;
                      
-                        if ( Math.abs(s.delta) >= s.scrollThreshold) {
-                          SliceSlider.prevSlide();
-                        }
-                      }
+                    //     if ( Math.abs(s.delta) >= s.scrollThreshold) {
+                    //       SliceSlider.prevSlide();
+                    //     }
+                    //   }
                  
-                      // Scrolling Down
-                      else {
+                    //   // Scrolling Down
+                    //   else {
                  
-                        s.delta++;
+                    //     s.delta++;
                  
-                        if (s.delta >= s.scrollThreshold) {
-                          SliceSlider.nextSlide();
-                        }
-                      }
+                    //     if (s.delta >= s.scrollThreshold) {
+                    //       SliceSlider.nextSlide();
+                    //     }
+                    //   }
                  
-                      // Prevent page from scrolling
-                      return false;
+                    //   // Prevent page from scrolling
+                    //   return false;
                     },
                 
                     /**
